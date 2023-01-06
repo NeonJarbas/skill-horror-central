db4e267d01c160d4cadbf7ac8f0d6bb388624460
jarbasai <jarbasai@mailfence.com>
2023-01-06 12:39:10 +0000

    scrap automation

--- __init__.py
+++ __init__.py
@@ -23,7 +23,7 @@ class HorrorCentralSkill(OVOSCommonPlaybackSkill):
 
     def initialize(self):
         url = "https://www.youtube.com/channel/UC_MyK1SmeN2Sji4uBEckYaA"
-        bootstrap = f"https://raw.githubusercontent.com/OpenJarbas/streamindex/main/{self.archive.db.name}.json"
+        bootstrap = "https://github.com/JarbasSkills/skill-horror-central/raw/dev/bootstrap.json"
         self.archive.bootstrap_from_url(bootstrap)
         self.archive.monitor(url)
         self.archive.setDaemon(True)
